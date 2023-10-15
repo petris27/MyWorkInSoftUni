cc3e1ab95b0e9a90e22e14232cd8aed42397e4f0
petris27 <vladi_milkov@abv.bg>
2023-10-15 15:23:41 +0300

    21.09.2023

---
+++ Programming Fundamentals with Python - септември 2023/exercises and homeworks 19.09 - 21.09/Sum of chars.py
@@ -0,0 +1,6 @@
+number_of_charecters = int(input())
+total_sum = 0
+for character in range(number_of_charecters):
+    current_character = input()
+    total_sum += ord(current_character)
+print(f"The sum equals: {total_sum}")
