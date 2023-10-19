6f47ff75f7259f5fd9b16af55a077e5c2d33e0b3
petris27 <vladi_milkov@abv.bg>
2023-10-19 11:50:44 +0300

    26.09 - 29.09

---
+++ Programming Fundamentals with Python - септември 2023/exercises and homework 26.09-29.09/Courses.py
@@ -0,0 +1,7 @@
+number_of_courses = int(input())
+cour = []
+for courses in range(number_of_courses):
+    courses = input()
+    cour.append(courses)
+
+print(cour)
---
+++ Programming Fundamentals with Python - септември 2023/exercises and homework 26.09-29.09/Invert Values.py
@@ -0,0 +1,6 @@
+number = input().split()
+my_list = []
+for n in number:
+    current_number = -int(n)
+    my_list.append(current_number)
+print(my_list)
