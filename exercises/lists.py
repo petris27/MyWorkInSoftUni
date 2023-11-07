26cecc08051f77691da24236eb5025432b414d98
petris27 <vladi_milkov@abv.bg>
2023-11-07 16:22:43 +0200

    practise
    Doing exercises on lessons.

---
+++ exercises/lists.py
@@ -0,0 +1,32 @@
+#упражняване на листове - правене, принтиране на даден елемент,
+#добавяне, махане, проверяване на дължина, ползване на for и while,
+#и др.
+
+
+#лист с добавяне, махане, for loop и сортиране в азбучен ред.
+
+#list = ["banana", "cherry", "strawberry", "peach"]
+#list.append("apple")
+#list.remove("peach")
+#list.sort()
+#for element in list:
+#    print(element)
+
+#обръщане реда на принтиране на елементите от горния код
+
+#list = ["banana", "cherry", "strawberry", "peach"]
+#list.append("apple")
+#list.remove("peach")
+#list.sort()
+#list.reverse()
+#for element in list:
+#    print(element)
+
+#добавяне на лист към друг лист
+
+#list = ["banana", "cherry", "strawberry", "peach"]
+#listo = ["cucumber", "tomato", "potato"]
+#list.append("apple")
+#list.remove("peach")
+#list.append(listo)
+#print(list)
