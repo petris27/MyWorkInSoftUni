d4063f05f1bf20741ef8a54d853c923118fabd79
petris27 <vladi_milkov@abv.bg>
2024-02-10 14:40:25 +0200

    Multidimensional Lists - Lab

--- Pyrhon advanced/Multidimensional Lists/Square with Maximum Sum.py
+++ Pyrhon advanced/Multidimensional Lists/Square with Maximum Sum.py
@@ -11,8 +11,8 @@ sub_matrix = []
 for row_index in range(row-1):
     for col_index in range(col-1):
         current_element = matrix[row_index][col_index]
-        next_element = matrix[row_index][col_index]
-        element_under = matrix[row_index][col_index]
+        next_element = matrix[row_index][col_index+1]
+        element_under = matrix[row_index+1][col_index]
         element_diagonal = matrix[row_index+1][col_index+1]
 
         total_sum = current_element + next_element + element_under + element_diagonal
@@ -21,7 +21,7 @@ for row_index in range(row-1):
             max_sum = total_sum
             sub_matrix = [[current_element, next_element], [element_under, element_diagonal]]
 
-print(sub_matrix[0])
-print(sub_matrix[1])
+print(*sub_matrix[0])
+print(*sub_matrix[1])
 print(max_sum)
 
